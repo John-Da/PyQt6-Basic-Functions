1dd880415ce7a13bfd8604e1bcee60cdd49dad35
Da-Hwa <yenda.h@kkumail.com>
2024-06-20 11:42:30 +0700

    correcting code and create a numbers text file

--- exercise1/write_read_file.py
+++ exercise1/write_read_file.py
@@ -1,14 +1,26 @@
-number1 = float(input("Enter a number: "))
-number2 = float(input("Enter a number: "))
-
-print(f"{number1} + {number2} = {number1 + number2}")
 
 
-with open("numbers.txt" ,'w') as numb:
-     print(f'{number1} + {number2} = {number1+number2}', file=numb)
-     print('Writing to numbers.txt')
+textFile = 'numbers.txt'
 
-with open("numbers.txt" , "r" ) as readFile:
-     reading = readFile.read()
-     print('Reading from numbers.txt')
-     print(reading)
+
+
+first_number = float(input("Enter a number: "))
+second_number = float(input("Enter a number: "))
+
+
+print(f"{first_number} + {second_number} = {first_number + second_number}")
+
+with open("numbers.txt" ,'w') as numFile:
+     print(f'{first_number} + {second_number} = {first_number+second_number}', file=numFile)
+     print('Writing to file numbers.txt')
+
+
+with open("numbers.txt" , "r" ) as numFile:
+     data = numFile.read()
+     print('Reading from file numbers.txt')
+     print(data)
+
+
+
+
+     
