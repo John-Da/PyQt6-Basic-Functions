279775bafadb42d811ac738b883fbc5ae710c498
Da-Hwa <yenda.h@kkumail.com>
2024-06-20 21:22:13 +0700

    changing the variables and coding

--- exercise1/adder.py
+++ exercise1/adder.py
@@ -4,3 +4,5 @@ first_number = float(input("Enter a number:"))
 second_number = float(input("Enter a number:"))
 
 print(f'{first_number} + {second_number} = {first_number + second_number}')
+
+
--- exercise1/write_read_file.py
+++ exercise1/write_read_file.py
@@ -8,14 +8,16 @@ first_number = float(input("Enter a number: "))
 second_number = float(input("Enter a number: "))
 
 
-print(f"{first_number} + {second_number} = {first_number + second_number}")
+result = f"{first_number} + {second_number} = {first_number + second_number}"
+print(result)
 
 with open(textFile ,'w') as numFile:
-     print(f'{first_number} + {second_number} = {first_number+second_number}', file=numFile)
+     # print(result, file=numFile)
+     numFile.write(result)
      print('Writing to file numbers.txt')
 
 
-with open(textFile , "r" ) as numFile:
+with open(textFile , 'r' ) as numFile:
      data = numFile.read()
      print('Reading from file numbers.txt')
      print(data)
