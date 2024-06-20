ddc991ba80ebbe0251302feadb4832cf0eb097eb
Da-Hwa <yenda.h@kkumail.com>
2024-06-20 11:48:31 +0700

    changing variable names

--- exercise1/write_read_file.py
+++ exercise1/write_read_file.py
@@ -10,12 +10,12 @@ second_number = float(input("Enter a number: "))
 
 print(f"{first_number} + {second_number} = {first_number + second_number}")
 
-with open("numbers.txt" ,'w') as numFile:
+with open(textFile ,'w') as numFile:
      print(f'{first_number} + {second_number} = {first_number+second_number}', file=numFile)
      print('Writing to file numbers.txt')
 
 
-with open("numbers.txt" , "r" ) as numFile:
+with open(textFile , "r" ) as numFile:
      data = numFile.read()
      print('Reading from file numbers.txt')
      print(data)
