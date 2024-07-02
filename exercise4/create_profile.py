81c82a1045dbf1cc2c1428e1359fae0875a7d8b7
John-Da <john.213steven@gmail.com>
2024-07-02 10:05:35 +0700

    black formatted

--- exercise4/create_profile.py
+++ exercise4/create_profile.py
@@ -1,10 +1,7 @@
+def creat_profile(name, age, occupation="student"):
+    print(f"Name: {name}, Age: {age}, Occupation: {occupation}", end="")
 
 
-
-def creat_profile(name, age, occupation='student'):
-    print(f"Name: {name}, Age: {age}, Occupation: {occupation}")
-
-
-print(creat_profile('Alice', 25))
-print(creat_profile('Bob', 30, 'engineer'))
-print(creat_profile(age=22, name='Charlie', occupation='designer'))
+print(creat_profile("Alice", 25))
+print(creat_profile("Bob", 30, "engineer"))
+print(creat_profile(age=22, name="Charlie", occupation="designer"))
