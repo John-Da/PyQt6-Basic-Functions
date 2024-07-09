6052322dad5460403ee175bc99f8c4f2332dc69a
Mee Too <john.213steven@gmail.com>
2024-07-09 11:27:27 +0700

    last submission

--- exercise6/calculate_average.py
+++ exercise6/calculate_average.py
@@ -1,15 +1,12 @@
 def calculate_average(*numbers, round_to=2):
-    number = len(numbers)
-    if number == 0:
-        return 0
-    else:
+    try:
         avg = sum(numbers) / len(numbers)
         answer = round(avg, round_to) 
         return answer
-
-
-
-
+    except ZeroDivisionError:
+        return 0
+        
+        
 print(calculate_average(100, 120, 130))
 print(calculate_average(10.5, 20.5, 30.5, round_to=1))
 print(calculate_average())
