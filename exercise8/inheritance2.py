8fe66e98d034917169789bf08ade9090730325e3
Da Hwa <john.213steven@gmail.com>
2024-07-23 12:24:20 +0700

    exercise8 extra

--- exercise8/inheritance2.py
+++ exercise8/inheritance2.py
@@ -7,8 +7,8 @@ class Car:
         self._mileage = mileage
 
 
-    def drive(self, drive_distance):
-        self._mileage = drive_distance
+    def drive(self, distance):
+        self._mileage = distance
 
     def __str__(self):
         return f"{self._year} {self._make} {self._model}, Mileage: {self._mileage}"
@@ -20,24 +20,26 @@ class RentalCar(Car):
         self._totalCost = 0
         self._is_rented = False
         self._dailyRate = dailyrate
-        self._returnCar = False
 
 
     def rent(self, days):
-        if self._is_rented:
-            self._is_rented = 'Rented'
-            return f"{super().__str__()}, Daily rate: ${self._dailyRate}, Status: {self._is_rented}"
-        else:
-            self._is_rented = 'Available'
+        self._is_rented = True
         self._totalCost = days * self._dailyRate
-        return f"Rented for {days} days. Toatal cost: ${self._totalCost}"
+        return f"Rented for {days} days. Total cost: ${self._totalCost}"
 
-    def return_car(self, mileage): 
-        self._mileage += mileage
-        return f"Car retured."
+    def return_car(self, distance_driven): 
+        self._mileage += distance_driven
+        self._is_rented = False
+        return f"Car returned."
 
     def __str__(self):
-        return f"{super().__str__()}, Daily rate: ${self._dailyRate}, Status: {self._is_rented}"
+        if self._is_rented:
+            rented = 'Rented'
+            return f"{super().__str__()}, Daily rate: ${self._dailyRate}, Status: {rented}"
+
+        else:
+            avialble = 'Available'
+            return f"{super().__str__()}, Daily rate: ${self._dailyRate}, Status: {avialble}"
 
 
 civic = Car("Honday", "Civic", 2021)
@@ -46,5 +48,6 @@ print(civic)
 rental_car = RentalCar("Toyota", "Camry", 2022, 50)
 print(rental_car)
 print(rental_car.rent(3))
+print(rental_car)
 print(rental_car.return_car(100))
 print(rental_car)
