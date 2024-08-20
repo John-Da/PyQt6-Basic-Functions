22a28b25ffff2b02064ffc815fb91d4c323c174e
Da Hwa <john.213steven@gmail.com>
2024-08-20 12:14:50 +0700

    exercise12

---
+++ exercise12/prob1.py
@@ -0,0 +1,73 @@
+
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
+import sys
+
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+        self.setFixedSize(QSize(500, 200))
+
+        self.label = QLabel()
+        font = self.label.font()
+        font.setPointSize(10)
+        self.label.setFont(font)
+        self.label.setStyleSheet('background:yellow; color:black;')
+
+        self.input = QLineEdit()
+        self.input.textChanged.connect(self.change_Text)
+
+
+
+        self.listWidget = QListWidget()
+        self.listWidget.setSelectionMode(QAbstractItemView.SelectionMode.MultiSelection)
+
+        itemlist = ['EN842300', 'EN842314', 'EN842315']
+        for i in range(len(itemlist)):
+            items = QListWidgetItem(itemlist[i])
+            self.listWidget.addItem(items)
+        self.listWidget.itemClicked.connect(self.printItemText)
+
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.listWidget)
+        layout.addWidget(self.label)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
+
+
+    def change_Text(self):
+        
+        inputtext = self.input.text()
+        hello = f'Hello {inputtext}!'
+        return self.label.setText(hello)
+
+    def printItemText(self):
+        inputtext = self.input.text()
+        items = self.listWidget.selectedItems()
+        x = []
+        for i in range(len(items)):
+            x.append(str(self.listWidget.selectedItems()[i].text()))
+        return self.label.setText(f'Hello {inputtext}! You are interested in these courses {",".join(map(str, x))}')
+        
+
+
+
+
+        
+
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
---
+++ exercise12/prob2.py
@@ -0,0 +1,73 @@
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Name Form")
+        self.setFixedSize(QSize(600, 200))
+
+
+        self.label1 = QLabel('First Name:')
+        font = self.label1.font()
+        font.setPointSize(20)
+        self.label1.setFont(font)
+
+        self.label2 = QLabel('Last Name:')
+        font = self.label2.font()
+        font.setPointSize(20)
+        self.label2.setFont(font)
+
+        self.input1 = QLineEdit()
+
+        self.input2 = QLineEdit()
+
+
+
+        self.cancel = QPushButton('Cancel')
+        font = self.cancel.font()
+        font.setPointSize(20)
+        self.cancel.setFont(font)
+        self.submit = QPushButton('Submit')
+        font = self.submit.font()
+        font.setPointSize(20)
+        self.submit.setFont(font)
+
+
+        layout = QGridLayout()
+        layout.addWidget(self.label1, 0, 0)
+        layout.addWidget(self.label2, 1, 0)
+        layout.addWidget(self.input1, 0, 3)
+        layout.addWidget(self.input2, 1, 3)
+
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(self.cancel)
+        button_layout.addWidget(self.submit)
+
+        overall = QVBoxLayout()
+        overall.addLayout(layout)
+        overall.addLayout(button_layout)
+
+
+        container = QWidget()
+        container.setLayout(overall)
+        
+
+        self.setCentralWidget(container)
+
+        
+
+        
+
+
+
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
