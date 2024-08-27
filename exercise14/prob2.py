2e2411f1bb9f861905576250fddf0268a62d855a
Da Hwa <john.213steven@gmail.com>
2024-08-27 11:48:15 +0700

    exercise 14

---
+++ exercise14/prob2.py
@@ -0,0 +1,95 @@
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Simple Calculator")
+        self.setFixedSize(QSize(400, 500))
+        self._createMenuBar()
+        self.layouts = QVBoxLayout()
+
+        fNum_layout = QHBoxLayout()
+        fNum_layout.addWidget(QLabel("Enter the first number:"))
+        self.fNum_input = QLineEdit()
+        fNum_layout.addWidget(self.fNum_input)
+        self.fNum_input.setMaximumWidth(200)
+        self.fNum_input.setStyleSheet("background:yellow; color:#000")
+        self.fNum_input.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.layouts.addLayout(fNum_layout)
+
+        lNum_layout = QHBoxLayout()
+        lNum_layout.addWidget(QLabel("Enter the second number:"))
+        self.lNum_input = QLineEdit()
+        self.lNum_input.setMaximumWidth(200)
+        lNum_layout.addWidget(self.lNum_input)
+        self.lNum_input.setStyleSheet("background:yellow; color:#000")
+        self.lNum_input.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.layouts.addLayout(lNum_layout)
+
+        self.btns = ["+", "-", "*", "/"]
+        self._createBtn()
+        self.resultList = []
+        self._createResultLayout()
+
+        container = QWidget()
+        container.setLayout(self.layouts)
+
+        self.setCentralWidget(container)
+
+    def _createMenuBar(self):
+        self.menuBar = self.menuBar()
+        self.menuBar.setNativeMenuBar(False)
+        self.fileMenu = self.menuBar.addMenu("File")
+        self.editMenu = self.menuBar.addMenu("Edit")
+        self.configMenu = self.menuBar.addMenu("Config")
+
+    def _createBtn(self):
+        btn_layout = QHBoxLayout()
+        for btn in self.btns:
+            self.operators = QPushButton(btn)
+            self.operators.setMaximumWidth(160)
+            self.operators.clicked.connect(self.calculate)
+            btn_layout.addWidget(self.operators)
+        self.layouts.addLayout(btn_layout)
+
+    def _createResultLayout(self):
+
+        result_layout = QHBoxLayout()
+        result_layout.addWidget(QLabel("Resutl:"))
+        self.result_label = QTextEdit()
+        self.result_label.setStyleSheet("background:lightgreen; color:#000")
+        result_layout.addWidget(self.result_label)
+        self.layouts.addLayout(result_layout)
+
+    def calculate(self):
+        inputs = self.sender()
+        op = inputs.text()
+        try:
+            num1 = int(self.fNum_input.text())
+            num2 = int(self.lNum_input.text())
+            result = eval(f"{num1} {op} {num2}")
+
+            self.resultList.append(f"{num1} {op} {num2} = {result}")
+            self.result_label.setText("\n".join(map(str, self.resultList)))
+            self.clear_inputs()
+
+        except ValueError:
+            self.result_label.setText("Error: Invalid input")
+
+        except ZeroDivisionError:
+            self.result_label.setText("Error: Division by zero")
+
+    def clear_inputs(self):
+        self.fNum_input.clear()
+        self.lNum_input.clear()
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
