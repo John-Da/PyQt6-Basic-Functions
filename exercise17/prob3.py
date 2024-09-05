7d7a9bf42d948addebc7c2e78016320f4ca5c020
Da Hwa <john.213steven@gmail.com>
2024-09-05 16:54:35 +0700

    fixed

--- exercise17/prob3.py
+++ exercise17/prob3.py
@@ -14,22 +14,28 @@ class RobustCalculator(MessageBoxDisplay):
         super().__init__()
         self.setWindowTitle("Robust Calculator")
 
-        self.openAction.triggered.connect(self._openFile)
+        self.openAction.triggered.connect(self._openFileaa)
 
-    def _openFile(self):
+    def _openFileaa(self):
         path = os.path.dirname(__file__)
         os.chdir(path)
         filename = "result.txt"
         self.filePath = os.path.realpath("result.txt")
 
+        self.inputList = []
+
         try:
             with open(filename, "r") as results:
                 resultText = results.read()
                 self.result_label.setText(resultText)
-            self.messages = QMessageBox()
-            self.messages.setText(f"Reading result to file {self.filePath}")
-            self.messages.exec()
-            return
+
+                lines = resultText.splitlines()
+                for word in lines:
+                    num = word.split()
+                    first_num = num[0]
+                    sec_num = num[2]
+                self.fNum_input.setText(first_num)
+                self.lNum_input.setText(sec_num)
 
         except FileNotFoundError:
             self.messages = QMessageBox()
